a2a940223f26059980984d74d7dfc24eabbe1bbe
Pavel Kondratyev <shamurai@mail.ru>
2014-06-10 12:48:53 +0300

    initial commit

---
+++ CCURLConnection/CCURLConnection.h
@@ -0,0 +1,13 @@
+//
+//  CCURLConnection.h
+//  CCURLConnection
+//
+//  Created by Kondratyev, Pavel on 6/10/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCURLConnection : NSObject
+
+@end
---
+++ CCURLConnection/CCURLConnection.m
@@ -0,0 +1,13 @@
+//
+//  CCURLConnection.m
+//  CCURLConnection
+//
+//  Created by Kondratyev, Pavel on 6/10/14.
+//
+//
+
+#import "CCURLConnection.h"
+
+@implementation CCURLConnection
+
+@end
